49af80eb70a2a62a6a073b60a6eea6140bed6c61
Parinaz11 <parinaz.akef@gmail.com>
2024-08-11 08:09:08 +0330

    almost the end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent, // component: The Angular component that should be rendered when the route is activated
+    },
+    // how you would lazy load a module when accessing a single route
+    {
+        path: 'about-us',
+        loadChildren: () =>
+            import('./modules/about-us/about-us.module').then((m) => m.AboutUsModule),
+    }
+];
+
+// This keyword makes the routes (which is a constant and cannot be changed) available for import in other modules or files
+
